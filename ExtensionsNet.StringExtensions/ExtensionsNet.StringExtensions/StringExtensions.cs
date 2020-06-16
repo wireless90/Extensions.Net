54f3df3740d29f846636b629266028e430b7276b
Muhammad Razali <wireless-@hotmail.com>
2020-06-16 18:37:45 +0800

    Created Function: Between

--- ExtensionsNet.StringExtensions/ExtensionsNet.StringExtensions/StringExtensions.cs
+++ ExtensionsNet.StringExtensions/ExtensionsNet.StringExtensions/StringExtensions.cs
@@ -1,6 +1,31 @@
-﻿namespace ExtensionsNet.StringExtensions
+﻿using System;
+
+namespace ExtensionsNet.StringExtensions
 {
     public static class StringExtensions
     {
+        private const int NOT_FOUND = -1;
+
+        public static String Between(this String value, String leftString, String rightString)
+        {
+            int leftIndex = value.IndexOf(leftString);
+
+            if(leftIndex == NOT_FOUND)
+            {
+                return String.Empty;
+            }
+
+            int rightIndex = value.LastIndexOf(rightString);
+
+            if(rightIndex == NOT_FOUND || (leftIndex + leftString.Length -1) == rightIndex)
+            {
+                return String.Empty;
+            }
+
+            int leftSubstringIndex = leftIndex + leftString.Length;
+            int rightSubstringIndex = rightIndex - leftSubstringIndex;
+
+            return value.Substring(leftSubstringIndex, rightSubstringIndex);
+        }
     }
 }
